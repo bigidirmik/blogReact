e824a9535b7ca26a3d739fd6187700c31c278d4c
Bilal Dırmık <bigisivri@outlook.com>
2021-09-07 14:22:43 +0300

    netlify

--- src/services/imageService.js
+++ src/services/imageService.js
@@ -2,22 +2,22 @@ import axios from "axios";
 
 export default class ImageService {
   add(postId, url) {
-    return axios.post("http://https://blog-spring-github.herokuapp.com/api/images/add?postId="+postId+"&url="+url);
+    return axios.post("https://blog-spring-github.herokuapp.com/api/images/add?postId="+postId+"&url="+url);
   }
 
   update(postId, url) {
-    return axios.put("http://https://blog-spring-github.herokuapp.com/api/images/update?postId="+postId+"&url="+url);
+    return axios.put("https://blog-spring-github.herokuapp.com/api/images/update?postId="+postId+"&url="+url);
   }
 
   deleteById(imageId) {
     return axios.delete(
-      "http://https://blog-spring-github.herokuapp.com/api/images/delete?imageId=" + imageId
+      "https://blog-spring-github.herokuapp.com/api/images/delete?imageId=" + imageId
     );
   }
 
   getByPostId(postId) {
     return axios.get(
-      "http://https://blog-spring-github.herokuapp.com/api/images/getByPostId?postId=" + postId
+      "https://blog-spring-github.herokuapp.com/api/images/getByPostId?postId=" + postId
     );
   }
 }
